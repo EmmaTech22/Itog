210731f9acd8a6b546dec918a3e56e9e7c0a6b26
emma <emma_malonovaya@mail.ru>
2023-01-16 16:21:33 +0300

    add DZ_itog

---
+++ DZ_Itog/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string[] array = {"miu","cat","hello","love","world"};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.Write(string.Join(Environment.NewLine, result, 0, realSize));
